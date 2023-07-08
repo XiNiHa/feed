8ebeb133c1ecfa1fd95f75ccd3ba2c533cab2f0c
XiNiHa <me@xiniha.dev>
2023-07-08 23:23:53 +0900

    chore: init repository with common utilities

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/* eslint-env node */
+
+/** @type{import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/require-await': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'index', 'sibling'],
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'import/consistent-type-specifier-style': ['warn', 'prefer-top-level'],
+  },
+}
